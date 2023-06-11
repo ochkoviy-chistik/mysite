c7a519ae4115fa43c0a28d2ca59784aae89671ab
prom <topki4uk@mail.ru>
2023-06-11 20:03:10 +0300

    изменил структуру проекта

---
+++ boost/app/urls.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from app import views
+from app.ajax_queryes import ajax_notes
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.index),
+
+    path('id<int:pk>/', login_required(views.profile, login_url='/login/'), name='profile'),
+    path('id<int:pk>/edit/', login_required(views.profile_edit, login_url='/login/'), name='edit'),
+    path('bookmarks/', login_required(views.bookmarks, login_url='/login/'), name='bookmarks'),
+
+    path('document<int:pk>/', login_required(views.doc_page, login_url='/login/'), name='doc_page'),
+    path('document<int:pk>/edit/', login_required(views.doc_page_edit, login_url='/login/'), name='doc_page'),
+    path('create/', login_required(views.create_docs, login_url='/login/'), name='create_docs'),
+
+    path('document<int:pk>/getdata', ajax_notes.get_data, name='data_get_ajax'),
+    path('documents/postlikedata', ajax_notes.like_post, name='like_post_ajax'),
+    path('documents/postdislikedata', ajax_notes.dislike_post, name='dislike_post_ajax'),
+    path('documents/postcommentdata', ajax_notes.comment_post, name='comment_post_ajax'),
+    path('documents/postbookmarkdata', ajax_notes.bookmark_post, name='bookmark_post_ajax'),
+    path('documents/deletecomment', ajax_notes.delete_comment, name='delete_comment_ajax'),
+]
--- boost/boost/settings.py
+++ boost/boost/settings.py
@@ -14,7 +14,7 @@ import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-env = dotenv.dotenv_values('.env')
+env = dotenv.dotenv_values(r'.env')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
